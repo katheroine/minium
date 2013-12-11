764060d1d38d811f5b46d9aef15dde8a0987d781
Katarzyna Krasińska <katheroine@gmail.com>
2013-12-11 13:40:05 +0100

    create_photos_table migration added

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'minium',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'pstihearahacker',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
