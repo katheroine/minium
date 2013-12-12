abba009d4ef6194413694d8861074dc243cec4c7
Katarzyna Krasińska <katheroine@gmail.com>
2013-12-12 14:24:48 +0100

    photos index view fixed

--- app/views/photos/index.blade.php
+++ app/views/photos/index.blade.php
@@ -31,6 +31,7 @@
 			<td>Title</td>
 			<td>Description</td>
 			<td>Order</td>
+			<td>Actions</td>
 		</tr>
 	</thead>
 	<tbody>
@@ -48,10 +49,10 @@
 				<!-- we will add this later since its a little more complicated than the other two buttons -->
 
 				<!-- show the nerd (uses the show method found at GET /nerds/{id} -->
-				<a class="btn btn-small btn-success" href="{{ URL::to('photos/' . $value->id) }}">Show this Photo</a>
+				<a class="btn btn-small btn-success" href="{{ URL::to('photos/' . $photo->id) }}">Show this Photo</a>
 
 				<!-- edit this nerd (uses the edit method found at GET /nerds/{id}/edit -->
-				<a class="btn btn-small btn-info" href="{{ URL::to('photos/' . $value->id . '/edit') }}">Edit this Photo</a>
+				<a class="btn btn-small btn-info" href="{{ URL::to('photos/' . $photo->id . '/edit') }}">Edit this Photo</a>
 
 			</td>
 		</tr>
