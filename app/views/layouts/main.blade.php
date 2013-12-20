739980f23619d5432bdf7790c08ba9edb99b07db
Katarzyna Krasińska <katheroine@gmail.com>
2013-12-20 08:21:12 +0100

    layouts.main view added

---
+++ app/views/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Minium</title>
+	{{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css') }}
+	{{ HTML::script('//code.jquery.com/jquery-1.10.1.min.js') }}
+	{{ HTML::script('//code.jquery.com/jquery-migrate-1.2.1.min.js') }}
+	{{ HTML::style('//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.css') }}
+	{{ HTML::script('//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.js') }}
+</head>
+<body>
+<div class="container">
+	@yield('content')
+</div>
+</body>
+</html>
