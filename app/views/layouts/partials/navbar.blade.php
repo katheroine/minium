89173f0e51e96a0888bfbfb2ece4948d2caa36ef
Katarzyna Krasińska <katheroine@gmail.com>
2013-12-21 18:07:07 +0100

    layouts.partials.navbar view modified; photo categories links added

--- app/views/layouts/partials/navbar.blade.php
+++ app/views/layouts/partials/navbar.blade.php
@@ -5,6 +5,8 @@
 	<ul class="nav navbar-nav">
 		<li><a href="{{ URL::to('photos') }}">View All Photos</a></li>
 		<li><a href="{{ URL::to('photos/create') }}">Upload a Photo</a>
+		<li><a href="{{ URL::to('photo_categories') }}">View All Photo categories</a></li>
+		<li><a href="{{ URL::to('photo_categories/create') }}">Add a Photo category</a>
 	</ul>
 </nav>
 
