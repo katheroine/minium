6c0dc7b7b21da54faaaec155d4f5deb47c8c1aae
Katarzyna Krasińska <katheroine@gmail.com>
2013-12-30 09:27:33 +0100

    PhotoCategory moel modified; allNames method added

--- app/models/PhotoCategory.php
+++ app/models/PhotoCategory.php
@@ -6,4 +6,24 @@ class PhotoCategory extends Eloquent {
 	{
 		return $this->hasMany('Photo');
 	}
+	
+	/**
+	 * Returns array with model item IDs as keys and names as values.
+	 * 
+	 * @return array
+	 */
+	static public function allNames()
+	{	
+		$categories = PhotoCategory::all();
+		
+		$names = array();
+		
+		$categories->map(function($value) use (&$names)
+		{
+			$id = $value['id'];
+			$names[$id] = $value['name'];
+		});
+		
+		return $names;
+	}
 }
