361ab471b228e780554a126c61e0b8d1c3b8f887
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-14 10:09:38 +0100

    layouts.partials.menubar partial view added

---
+++ app/views/layouts/partials/menubar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-inverse">
+	<div class="navbar-header">
+		<a class="navbar-brand" href="{{ URL::to('/') }}">Minium</a>
+	</div>
+	<ul class="nav navbar-nav">
+		<li><a href="{{ URL::to('/') }}">Home</a></li>
+		<li><a href="{{ URL::to('/about') }}">About</a>
+	</ul>
+</nav>
