9a935c3657925abdc1453ec950051b12d42beaca
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-17 21:02:58 +0100

    home.index view modified; image slider for favourite photos added

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.home')
 
+@section('assets')
+
+{{ HTML::script('//code.jquery.com/jquery-1.10.1.min.js') }}
+{{ HTML::script('//code.jquery.com/jquery-migrate-1.2.1.min.js') }}
+{{ HTML::style('assets/css/home.index.css') }}
+{{ HTML::script('assets/js/home.index.js') }}
+
+@stop
+
 @section('content')
 
-
+<div id="gallery">
+	<div id="gallery_slider">
+	@foreach($favourite_photos as $photo)
+		{{ HTML::image($photo->file_path, '', array('id' => $photo->id, 'class' => 'slider')) }}
+	@endforeach
+	</div>
+	<a id="prev"><</a><a id="next">></a>
+</div>
 
 @stop
