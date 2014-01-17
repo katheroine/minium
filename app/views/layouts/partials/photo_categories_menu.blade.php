b685d2aecfb2ee295f26d5c2cbc62ea2eb36aefb
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-17 20:28:57 +0100

    layouts.partials.photo_categories_menu partial view modified; css class changed

--- app/views/layouts/partials/photo_categories_menu.blade.php
+++ app/views/layouts/partials/photo_categories_menu.blade.php
@@ -1,4 +1,4 @@
-<div class="navbar-brand">
+<div class="navbar">
 @foreach($photo_categories as $photo_category)
 	{{ HTML::link('/photo_category/' . $photo_category->id, $photo_category->name) }}
 @endforeach
