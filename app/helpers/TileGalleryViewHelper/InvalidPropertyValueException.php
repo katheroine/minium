9dc90b0c6334ad883ec46121ef52fcc1ed35f6de
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-30 13:36:37 +0100

    InvalidPropertyValueException exception class added in TileGalleryViewHelper view helper

---
+++ app/helpers/TileGalleryViewHelper/InvalidPropertyValueException.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Invalid parameter type exception indicates that 
+ * property of invalid type or with invalid value has been given.
+ */
+class InvalidPropertyValueException extends Exception {
+}
