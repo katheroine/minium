bfffa725da16e606d8a4c22f520e9251ff467927
Katarzyna Krasińska <katheroine@gmail.com>
2014-01-31 13:41:34 +0100

    layouts.partials.navbar view modified; URLs prefixed with 'admin'

--- app/views/layouts/partials/navbar.blade.php
+++ app/views/layouts/partials/navbar.blade.php
@@ -3,10 +3,10 @@
 		<a class="navbar-brand" href="{{ URL::to('/') }}">Minium</a>
 	</div>
 	<ul class="nav navbar-nav">
-		<li><a href="{{ URL::to('photos') }}">View All Photos</a></li>
-		<li><a href="{{ URL::to('photos/create') }}">Upload a Photo</a>
-		<li><a href="{{ URL::to('photo_categories') }}">View All Photo categories</a></li>
-		<li><a href="{{ URL::to('photo_categories/create') }}">Add a Photo category</a>
+		<li><a href="{{ URL::to('admin/photos') }}">View All Photos</a></li>
+		<li><a href="{{ URL::to('admin/photos/create') }}">Upload a Photo</a>
+		<li><a href="{{ URL::to('admin/photo_categories') }}">View All Photo categories</a></li>
+		<li><a href="{{ URL::to('admin/photo_categories/create') }}">Add a Photo category</a>
 	</ul>
 </nav>
 
